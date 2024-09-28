ca4cc16d74428efd61662ddf5ca19f4ab4c3a605
KarlisJ <karlis.juskovs@gmail.com>
2024-09-28 16:14:06 +0300

    clean up

--- src/tests/TestControllerCase.php
+++ src/tests/TestControllerCase.php
@@ -7,16 +7,21 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use Illuminate\Support\Facades\Artisan;
 
-abstract class TestControllerCase extends OrchestraTestCase
+abstract class TestControllerCase extends BaseTestCase
 {
     use RefreshDatabase;
 
-    protected function getPackageProviders($app): array
-    {
-        return [
-            \Karlis\Module1\Module1ServiceProvider::class,
-        ];
-    }
+    // For easier local module testing orchestra/testbench has been added
+    // change BaseTestCase -> OrchestraTestCase
+    // uncomment getPackageProviders method
+    // and it will be possible to run tests on module1, while it's not attached to a full Laravel project
+
+//    protected function getPackageProviders($app): array
+//    {
+//        return [
+//            \Karlis\Module1\Module1ServiceProvider::class,
+//        ];
+//    }
 
     protected function setUp(): void
     {
