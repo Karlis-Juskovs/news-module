aeaf0b0fd1cc9595683f6f4f558953d71cee98ae
KarlisJ <karlis.juskovs@gmail.com>
2024-09-28 15:06:11 +0300

    updated module1 for separate testing option

--- src/tests/TestControllerCase.php
+++ src/tests/TestControllerCase.php
@@ -4,11 +4,19 @@ namespace Karlis\Module1\tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use Illuminate\Support\Facades\Artisan;
 
-abstract class TestControllerCase extends BaseTestCase
+abstract class TestControllerCase extends OrchestraTestCase
 {
     use RefreshDatabase;
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            \Karlis\Module1\Module1ServiceProvider::class,
+        ];
+    }
 
     protected function setUp(): void
     {
